803dec9680fc96f719ac920c6b27fcb1e4bc131b
talent-mu <624856021@qq.com>
2018-02-08 02:16:54 +0800

    用户注册

---
+++ src/main/java/com/shx/law/entity/Advice.java
@@ -0,0 +1,45 @@
+package com.shx.law.entity;
+
+import java.util.Date;
+
+public class Advice {
+    private Integer id;
+
+    private String content;
+
+    private Integer createUserid;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Integer getCreateUserid() {
+        return createUserid;
+    }
+
+    public void setCreateUserid(Integer createUserid) {
+        this.createUserid = createUserid;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
