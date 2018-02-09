ab36d97593de5cc0d31194b3903378f90f8eef8e
zhaomuhan <zhaomuhan@jd.com>
2018-02-09 18:37:22 +0800

    上传头像

--- src/main/java/com/shx/law/action/UserAction.java
+++ src/main/java/com/shx/law/action/UserAction.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -26,220 +27,251 @@ public class UserAction {
     private UserService userService;
     @Autowired
     private MessageService messageService;
+
     /**
      * 获取公司列表
+     *
      * @return
      */
     @RequestMapping("/getCompanyList")
     public @ResponseBody
     Response getCompanyList() {
-        List<Company> companyList= userService.getCompanyList();
-        HashMap result=new HashMap();
+        List<Company> companyList = userService.getCompanyList();
+        HashMap result = new HashMap();
         String json = JSON.toJSONString(companyList);
-        result.put("companyList",json);
+        result.put("companyList", json);
         return ResultUtil.buidSuccess(result);
     }
 
     /**
      * 获取部门列表
+     *
      * @param httpServletRequest
      * @return
      */
     @RequestMapping("/getDepartmentList")
-    public @ResponseBody Response getDepartmentList(HttpServletRequest httpServletRequest) {
-        String request=httpServletRequest.getParameter("data");
-        JSONObject requestObject=JSON.parseObject(request);
-        String companyId=requestObject.getString("companyId");
-        List<Department> departMentList= userService.getDepartmentList(companyId);
-        HashMap result=new HashMap();
+    public @ResponseBody
+    Response getDepartmentList(HttpServletRequest httpServletRequest) {
+        String request = httpServletRequest.getParameter("data");
+        JSONObject requestObject = JSON.parseObject(request);
+        String companyId = requestObject.getString("companyId");
+        List<Department> departMentList = userService.getDepartmentList(companyId);
+        HashMap result = new HashMap();
         String json = JSON.toJSONString(departMentList);
-        result.put("departmentList",json);
+        result.put("departmentList", json);
         return ResultUtil.buidSuccess(result);
     }
 
     /**
      * 获取职位列表
+     *
      * @return
      */
     @RequestMapping("/getJobList")
     public @ResponseBody
     Response getJobList() {
-        List<Job> jobList= userService.getJobList();
-        HashMap result=new HashMap();
+        List<Job> jobList = userService.getJobList();
+        HashMap result = new HashMap();
         String json = JSON.toJSONString(jobList);
-        result.put("jobList",json);
+        result.put("jobList", json);
         return ResultUtil.buidSuccess(result);
     }
 
     /**
      * 获取验证码
+     *
      * @param httpServletRequest
      * @return
      */
     @RequestMapping("/getVerifyCode")
-    public @ResponseBody Response getVerifyCode(HttpServletRequest httpServletRequest) {
-        String request=httpServletRequest.getParameter("data");
-        JSONObject requestObject=JSON.parseObject(request);
-        String phone=requestObject.getString("phone");
+    public @ResponseBody
+    Response getVerifyCode(HttpServletRequest httpServletRequest) {
+        String request = httpServletRequest.getParameter("data");
+        JSONObject requestObject = JSON.parseObject(request);
+        String phone = requestObject.getString("phone");
         userService.getVerifyCode(phone);
-        HashMap result=new HashMap();
+        HashMap result = new HashMap();
         return ResultUtil.buidSuccess(result);
     }
 
     /**
      * 验证用户是否已注册以及验证码是否正确
+     *
      * @param httpServletRequest
      * @return
      */
     @RequestMapping("/checkRegist")
-    public @ResponseBody Response checkRegist(HttpServletRequest httpServletRequest) {
+    public @ResponseBody
+    Response checkRegist(HttpServletRequest httpServletRequest) {
         try {
-            String request=httpServletRequest.getParameter("data");
-            JSONObject requestObject=JSON.parseObject(request);
-            String phone=requestObject.getString("phone");
-            String verifyCode=requestObject.getString("verifyCode");
-            userService.checkRegist(phone,verifyCode);
-            HashMap result=new HashMap();
+            String request = httpServletRequest.getParameter("data");
+            JSONObject requestObject = JSON.parseObject(request);
+            String phone = requestObject.getString("phone");
+            String verifyCode = requestObject.getString("verifyCode");
+            userService.checkRegist(phone, verifyCode);
+            HashMap result = new HashMap();
             return ResultUtil.buidSuccess(result);
         } catch (SystemException e) {
             e.printStackTrace();
-            return ResultUtil.buidFail(e.getMessage(),e.getCode());
+            return ResultUtil.buidFail(e.getMessage(), e.getCode());
         }
-
     }
 
     @RequestMapping("/regist")
-    public @ResponseBody Response list(HttpServletRequest httpServletRequest) {
+    public @ResponseBody
+    Response list(HttpServletRequest httpServletRequest) {
         try {
-            String request=httpServletRequest.getParameter("data");
-            UserRequest userRequest=JSON.parseObject(request,UserRequest.class);
+            String request = httpServletRequest.getParameter("data");
+            UserRequest userRequest = JSON.parseObject(request, UserRequest.class);
             userService.regist(userRequest);
-            HashMap result=new HashMap();
+            HashMap result = new HashMap();
             return ResultUtil.buidSuccess(result);
         } catch (Exception e) {
             e.printStackTrace();
-            return ResultUtil.buidFail(e.getMessage(),"10010");
+            return ResultUtil.buidFail(e.getMessage(), "10010");
         }
     }
+
     /**
      * 登录
+     *
      * @param httpServletRequest
      * @return
      */
     @RequestMapping("/login")
-    public @ResponseBody Response login(HttpServletRequest httpServletRequest) {
+    public @ResponseBody
+    Response login(HttpServletRequest httpServletRequest) {
         try {
-            String request=httpServletRequest.getParameter("data");
-            JSONObject requestObject=JSON.parseObject(request);
-            String phone=requestObject.getString("phone");
-            String verifyCode=requestObject.getString("verifyCode");
-            Map userInfo=userService.login(phone,verifyCode);
-            HashMap result=new HashMap();
+            String request = httpServletRequest.getParameter("data");
+            JSONObject requestObject = JSON.parseObject(request);
+            String phone = requestObject.getString("phone");
+            String verifyCode = requestObject.getString("verifyCode");
+            Map userInfo = userService.login(phone, verifyCode);
+            HashMap result = new HashMap();
             String json = JSON.toJSONString(userInfo);
-            result.put("userInfo",json);
+            result.put("userInfo", json);
             return ResultUtil.buidSuccess(result);
         } catch (SystemException e) {
             e.printStackTrace();
-            return ResultUtil.buidFail(e.getMessage(),e.getCode());
+            return ResultUtil.buidFail(e.getMessage(), e.getCode());
         }
-
     }
 
     /**
      * 获取用户信息
+     *
      * @param httpServletRequest
      * @return
      */
     @RequestMapping("/getUserInfo")
-    public @ResponseBody Response getUserInfo(HttpServletRequest httpServletRequest) {
+    public @ResponseBody
+    Response getUserInfo(HttpServletRequest httpServletRequest) {
         try {
-            String request=httpServletRequest.getParameter("data");
-            JSONObject requestObject=JSON.parseObject(request);
-            Integer userId=requestObject.getInteger("userId");
+            String request = httpServletRequest.getParameter("data");
+            JSONObject requestObject = JSON.parseObject(request);
+            Integer userId = requestObject.getInteger("userId");
 
-            Map userInfo=userService.getUserInfo(userId);
-            HashMap result=new HashMap();
+            Map userInfo = userService.getUserInfo(userId);
+            HashMap result = new HashMap();
             String json = JSON.toJSONString(userInfo);
-            result.put("userInfo",json);
+            result.put("userInfo", json);
             return ResultUtil.buidSuccess(result);
         } catch (SystemException e) {
             e.printStackTrace();
-            return ResultUtil.buidFail(e.getMessage(),e.getCode());
+            return ResultUtil.buidFail(e.getMessage(), e.getCode());
         }
-
     }
-
 
     /**
      * 获取消息列表
+     *
      * @param httpServletRequest
      * @return
      */
     @RequestMapping("/getMessage")
-    public @ResponseBody Response getMessage(HttpServletRequest httpServletRequest) {
+    public @ResponseBody
+    Response getMessage(HttpServletRequest httpServletRequest) {
         try {
-            String request=httpServletRequest.getParameter("data");
-            JSONObject requestObject=JSON.parseObject(request);
-            String type=requestObject.getString("type");
+            String request = httpServletRequest.getParameter("data");
+            JSONObject requestObject = JSON.parseObject(request);
+            String type = requestObject.getString("type");
 
-            List<Message> messageList=messageService.getMessage(type);
-            HashMap result=new HashMap();
+            List<Message> messageList = messageService.getMessage(type);
+            HashMap result = new HashMap();
             String json = JSON.toJSONString(messageList);
-            result.put("messageList",json);
+            result.put("messageList", json);
             return ResultUtil.buidSuccess(result);
         } catch (SystemException e) {
             e.printStackTrace();
-            return ResultUtil.buidFail(e.getMessage(),e.getCode());
+            return ResultUtil.buidFail(e.getMessage(), e.getCode());
         }
-
     }
+
     /**
      * 获取
+     *
      * @param httpServletRequest
      * @return
      */
     @RequestMapping("/getNewVersion")
-    public @ResponseBody Response getNewVersion(HttpServletRequest httpServletRequest) {
+    public @ResponseBody
+    Response getNewVersion(HttpServletRequest httpServletRequest) {
         try {
-            String request=httpServletRequest.getParameter("data");
-            JSONObject requestObject=JSON.parseObject(request);
-            String code=requestObject.getString("versionCode");
+            String request = httpServletRequest.getParameter("data");
+            JSONObject requestObject = JSON.parseObject(request);
+            String code = requestObject.getString("versionCode");
 
-            VersionManager versionManager=userService.getNewVersion(code);
-            if(versionManager==null){
-                return ResultUtil.buidFail("当前已经是最新版本","10005");
+            VersionManager versionManager = userService.getNewVersion(code);
+            if (versionManager == null) {
+                return ResultUtil.buidFail("当前已经是最新版本", "10005");
             }
-            HashMap result=new HashMap();
+            HashMap result = new HashMap();
             String json = JSON.toJSONString(versionManager);
-            result.put("versionInfo",json);
+            result.put("versionInfo", json);
             return ResultUtil.buidSuccess(result);
         } catch (SystemException e) {
             e.printStackTrace();
-            return ResultUtil.buidFail(e.getMessage(),e.getCode());
+            return ResultUtil.buidFail(e.getMessage(), e.getCode());
         }
-
     }
-
 
     /**
      * 更换手机号
+     *
      * @param httpServletRequest
      * @return
      */
     @RequestMapping("/changePhone")
-    public @ResponseBody Response changePhone(HttpServletRequest httpServletRequest) {
+    public @ResponseBody
+    Response changePhone(HttpServletRequest httpServletRequest) {
         try {
-            String request=httpServletRequest.getParameter("data");
-            JSONObject requestObject=JSON.parseObject(request);
-            String phone=requestObject.getString("phone");
-            String verifyCode=requestObject.getString("verifyCode");
-            userService.changePhone(phone,verifyCode);
-            HashMap result=new HashMap();
+            String request = httpServletRequest.getParameter("data");
+            JSONObject requestObject = JSON.parseObject(request);
+            String phone = requestObject.getString("phone");
+            String verifyCode = requestObject.getString("verifyCode");
+            userService.changePhone(phone, verifyCode);
+            HashMap result = new HashMap();
             return ResultUtil.buidSuccess(result);
         } catch (SystemException e) {
             e.printStackTrace();
-            return ResultUtil.buidFail(e.getMessage(),e.getCode());
+            return ResultUtil.buidFail(e.getMessage(), e.getCode());
         }
+    }
 
+    @RequestMapping("/uploadAvatar")
+    public @ResponseBody
+    Response uploadAvatar(HttpServletRequest httpServletRequest) {
+        try {
+            String request = httpServletRequest.getParameter("data");
+            JSONObject requestObject = JSON.parseObject(request);
+            Integer userId = requestObject.getInteger("userId");
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest)httpServletRequest;
+
+            HashMap result = new HashMap();
+            return ResultUtil.buidSuccess(result);
+        } catch (SystemException e) {
+            e.printStackTrace();
+            return ResultUtil.buidFail(e.getMessage(), e.getCode());
+        }
     }
 }
